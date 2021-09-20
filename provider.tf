08ac69fd2682ad13748857f5465f860f1205881f
ArelaPazos1099 <81729506+ArelaPazos1099@users.noreply.github.com>
2021-09-20 14:34:28 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,9 @@
 provider "ibm" {
   region = var.ibm_region
   ibmcloud_api_key = var.ibmcloud_api_key
-  version    = "1.21.2"
+  version    = "1.17.02"
+}
+
+terraform {
+  required_version = "~> 0.13.0"
 }
