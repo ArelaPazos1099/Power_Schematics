df396d258a3b83d0505732dc28c6228fb0708a26
ArelaPazos1099 <81729506+ArelaPazos1099@users.noreply.github.com>
2021-09-20 17:20:22 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,7 +3,6 @@ terraform {
   required_providers {
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "=1.27.1"
       region = var.ibm_region
       ibmcloud_api_key = var.ibmcloud_api_key
     }
