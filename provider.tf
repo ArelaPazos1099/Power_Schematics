1433839c065d86e20636c28248eaa5b597b224dc
Emily Franco <emily.franco@ibm.com>
2021-09-20 17:33:10 -0500

    paso1

--- provider.tf
+++ provider.tf
@@ -2,6 +2,7 @@ terraform {
   required_version = ">= 0.13.3"
   required_providers {
     ibm = {
+      version = "1.27.1"
       source  = "ibm-cloud/ibm"
       region = var.ibm_region
       ibmcloud_api_key = var.ibmcloud_api_key
